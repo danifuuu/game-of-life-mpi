42b85d1dd0c9d7720dd1c56868a739a8031ce813
danifuuu <dmfuentes2001@gmail.com>
2022-05-24 08:08:21 +0200

    añadida función getmax

--- include/utils.h
+++ include/utils.h
@@ -1,2 +1,3 @@
 char **allocate_memory(int rows, int cols);
 
+int get_max(int *values, int n);
--- src/utils.c
+++ src/utils.c
@@ -9,3 +9,16 @@ char **allocate_memory(int rows, int cols)
 
     return matrix;
 }
+
+int get_max(int *values, int n)
+{
+    int max = values[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (values[i] > max)
+            max = values[i];
+    }
+
+    return max;
+}
