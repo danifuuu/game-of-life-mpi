0b797e95d256ad82a8c5310cbab6cc2469b720d1
danifuuu <dmfuentes2001@gmail.com>
2022-05-31 17:28:26 +0200

    cambios en los parametros de la funcion game

--- include/game.h
+++ include/game.h
@@ -1,3 +1,10 @@
+#include <SDL2/SDL.h>
+#include <unistd.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_render.h>
+
+
 void find_neighbours(MPI_Comm comm_grid, int my_rank, int np_y, int np_x, int *left, int *right, int *top, int *bottom, int *topleft, int *topright, int *bottomleft, int *bottomright);
 
 int neighbourhood_sum(int outer_i, int outer_j);
@@ -10,4 +17,4 @@ void calculate_outer();
 
 void interchange_info(int np_y, int np_x, int left, int right, int top, int bottom, int topright, int topleft, int bottomright, int bottomleft, MPI_Comm comm_grid, int local_n_rows, int local_n_cols);
 
-void game(MPI_Comm comm_grid, int rank, int np_x, int np_y, int normal_cols, int max_cols, int normal_rows, int max_rows);
+void game(MPI_Comm comm_grid, SDL_Renderer *r,int rank, int np_x, int np_y, int normal_cols, int max_cols, int normal_rows, int max_rows);
